03b58c61c117528215538e64d8ba1432ecdeedd3
abdul rafay <abdulrafaycodeder@email.com>
2025-12-12 11:07:12 -0800

    start javascript class 7

---
+++ script.js
@@ -0,0 +1,13 @@
+// var score = 1.7;
+// var math = Math.round(score)
+// console.log(math)
+
+// let rafay = -6.9;
+// let marks = Math.round(rafay)
+// console.log(marks)
+
+// let uname = Math.round(uname);
+
+// let rounded = Math.round()
+// console.log(rounded)
+
