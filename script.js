e652bc5ef9920af894b8b464ce7521f2ed0d1c79
abdul rafay <abdulrafaycodeder@email.com>
2025-12-13 03:12:25 -0800

    complete half video javascript class 7

--- script.js
+++ script.js
@@ -9,3 +9,35 @@
 // a = Math.round(b)
 // console.log(a)
 
+// var rounded = Math.round(0.533);
+// console.log(rounded)
+
+// var round = Math.round(0.5656565)
+
+// var round = Math.ceil(2.2222)
+
+// // var round = Math.floor(2.2222)
+
+// console.log(round)
+
+
+// var round1 = Math.ceil(2.0)   // 3
+// var round2 = Math.ceil(2.9)   // 3
+// var round3 = Math.ceil(5.0)   // 5
+
+// console.log(round1)
+// console.log(round2)
+// console.log(round3)
+
+// var round4 = Math.floor(2.5)  // 2
+// var round5 = Math.floor(2.1)  // 2
+// var round6 = Math.floor(5.0)  // 5
+
+// console.log(round4)
+// console.log(round5)
+// console.log(round6)
+
+
+
+
+
