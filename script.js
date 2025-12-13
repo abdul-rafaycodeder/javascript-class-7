a22e286a26dab70b93aa0af9ceddfe122849c615
abdul rafay <abdulrafaycodeder@email.com>
2025-12-13 02:11:23 -0800

    start javascript class 7, day 2

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-// var score = 1.7;
-// var math = Math.round(score)
-// console.log(math)
+// let sum = 2.4;
+// let reslut = Math.random();
+// console.log(reslut);
 
-// let rafay = -6.9;
-// let marks = Math.round(rafay)
-// console.log(marks)
+// var uname = Math.round(Math.random() + 1);
+// console.log(uname);
 
-// let uname = Math.round(uname);
-
-// let rounded = Math.round()
-// console.log(rounded)
+// b = 2.5
+// a = Math.round(b)
+// console.log(a)
 
